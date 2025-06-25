021777e136ac546c323dbc24da056bbbf281561e
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-06-25 14:31:13 +0800

    build: deploy

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,23 +1,11 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/notes';
+const baseUrl = '/api/notes';
 
 const getAllNotes = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 const createNotes = (newObject) => {
   const request = axios.post(baseUrl, newObject);
