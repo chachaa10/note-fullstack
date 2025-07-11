d3b3d2f0aac43af41907bfcc513b4151d48fee0a
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-07-11 11:09:04 +0800

    fix: remove unnecessary send method

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -27,7 +27,7 @@ usersRouter.post('/', async (request, response) => {
 
   const savedUser = await user.save();
 
-  response.status(201).json(savedUser).send({ message: 'user created' });
+  response.status(201).json(savedUser);
 });
 
 usersRouter.delete('/:id', async (request, response) => {
